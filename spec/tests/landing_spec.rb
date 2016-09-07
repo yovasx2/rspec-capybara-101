4eb06633ccfc228e874eeed17b77372872d9ffa1
Giovanni Alberto <delirable@gmail.com>
2016-09-07 10:09:20 -0700

    Include TopNavigationBar, byebug and move loading path of ruby files
    The TopNavigationBar was created in order to wrapper the top bar,
    and have easy access to it.
    Add byebug in spec_helper via require.
    Change loading path to be recursive in ruby source files for
    page objects

--- spec/tests/landing_spec.rb
+++ spec/tests/landing_spec.rb
@@ -2,6 +2,22 @@ require 'spec_helper'
 
 describe 'Hi5 web site' do
 
+  describe 'on Top Navigation Bar' do
+    context 'with page object' do
+      before(:each) do
+        visit('/')
+        @top = TopNavigationBar.new
+      end
+
+      it 'shows error cause incorrect login' do
+        @top.do_login('user@user.com', 'password')
+        within_frame(find('#unified_login')) do
+          expect(page).to have_content('The email address or password you entered is incorrect. Please try again.')
+        end
+      end
+    end
+  end
+
   describe 'on Footer Navigation Bar ' do
     context 'without page object' do
       before(:each) do
